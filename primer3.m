73bbe41e350a2f2bc43498939fa00dadac1322f6
Katarina Černe <ktrn.crn@gmail.com>
2019-05-30 16:41:57 +0200

    končni popravki in morda še kaj, ne vem

--- primer3.m
+++ primer3.m
@@ -1,14 +1,14 @@
 function yprime = primer3(x,Y)
-beta1 = 100;
+beta1 = 20;
 beta2 = 2;
 d = 4;
 ni = 1;
-r1 = 1;
-r2 = 1;
-q = 0.5;
+r1 = 0.1;
+r2 = 0.2;
+q = 0.25;
 p = 1-q;
-b = 4;
-beta3 = 20000; 
+b = 40;
+beta3 = 200; 
 N = Y(1)+Y(2)+Y(3)+Y(4);
 yprime = [beta1*Y(3)*Y(2)/N+beta2*Y(4)*Y(2)/N-(d+ni+r1)*Y(1)+p*r2*Y(2)-beta3*Y(1)*Y(2)/N;
     ni*Y(1)-(d+r2)*Y(2)+ beta3*Y(1)*Y(2)/N;
--- primer4.m
+++ primer4.m
@@ -1,16 +1,17 @@
 function yprime = primer4(x,Y)
-beta1 = 200;
+beta1 = 20;
 beta2 = 2;
 d = 4;
 ni = 1;
-r1 = 1;
-r2 = 1;
-q = 0.5;
+r1 = 0.1;
+r2 = 0.2;
+q = 0.25;
 p = 1-q;
-b = 4;
+b = 40;
+beta3 = 200; 
 N = Y(1)+Y(2)+Y(3)+Y(4);
-yprime = [beta1*Y(3)*Y(2)/N+beta2*Y(4)*Y(2)/N-(d+ni+r1)*Y(1)+p*r2*Y(2);
-    ni*Y(1)-(d+r2)*Y(2);
+yprime = [beta1*Y(3)*Y(2)/N+beta2*Y(4)*Y(2)/N-(d+ni+r1)*Y(1)+p*r2*Y(2)-beta3*Y(1)*Y(2)/N;
+    ni*Y(1)-(d+r2)*Y(2)+ beta3*Y(1)*Y(2)/N;
     b-d*Y(3)-beta1*Y(3)*Y(2)/N;
     -d*Y(4)+r1*Y(1)+q*r2*Y(2)-beta2*Y(4)*Y(2)/N];
 end
